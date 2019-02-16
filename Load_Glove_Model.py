89cf18db07aae5bb916fd254e0411d0dbbe9af13
amit-kumar001 <47202519+amit-kumar001@users.noreply.github.com>
2019-02-16 15:20:50 +0530

    Create Load_Glove_Model.py

---
+++ Load_Glove_Model.py
@@ -0,0 +1,35 @@
+from glove import Corpus, Glove
+from sklearn.decomposition import PCA  # put this at the top of your program
+#to plot the graph using matplotlib 
+import matplotlib.pyplot as plt
+#load the glove model
+glove = Glove.load('glove.model')
+#vectors for each word 
+word2vec = glove.word_vectors
+#dictionary of the word
+dictionary = glove.dictionary
+print()
+print('dictionary',dictionary)
+print()
+#finding the one dimensional array for word hard
+array=  glove.word_vectors[glove.dictionary['hard']]
+print(word2vec.ndim)
+
+font = {'family': 'serif',
+        'color':  'darkred',
+        'weight': 'normal',
+        'size': 10,
+        }
+
+plt.text(0.5, -0.06, r'Word "hard" in vector form', fontdict=font)
+plt.plot(array)
+plt.grid(True)
+
+#finding the one dimensional array for word best
+array2 = glove.word_vectors[glove.dictionary['best']]
+plt.text(2.1, 0.050, r'Word "best" in vector form', fontdict=font)
+plt.plot(array2)
+plt.ylabel('Word Embedding', fontsize=12, color='red')
+plt.xlabel('X label', fontsize=12, color='red')
+plt.title(r'Vectors of words Glove',color='red')
+plt.show()
